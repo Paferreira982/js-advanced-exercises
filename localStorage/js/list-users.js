bfb32b5fde37f69c749665ea3128d1212554562e
Paferreira982 <paferreira982@hotmail.com>
2021-11-10 11:21:05 -0300

    Correcao no codigo de listar usuarios

--- localStorage/js/list-users.js
+++ localStorage/js/list-users.js
@@ -2,12 +2,8 @@ let users = JSON.parse(localStorage.getItem("users"));
 let allUsers = document.getElementById("all-users");
 let text = "";
 
-if (users == null) {
-    allUsers.innerHTML = "Nenhum usuário cadastrado";
-} else {
-    users.forEach(auxUser => {
-        text += "<br> Email: " + auxUser.email + " // Senha: " + auxUser.senha + " // Role: " + auxUser.role;
-    });
+users.forEach(auxUser => {
+    text += "<br> Id: " + auxUser.id + "// Email: " + auxUser.email + " // Senha: " + auxUser.senha + " // Role: " + auxUser.role;
+});
 
-    allUsers.innerHTML = text;
-}
+allUsers.innerHTML = text;
